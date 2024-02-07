43ee00604e17dd4cd30c344157e598d9ad6450b3
Russel Febrishky <russel.febrishky@myboost.co>
2024-02-07 14:09:07 +0800

    test initial commit

---
+++ app/src/main/java/com/rfgalahad/trinitywizardstestapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rfgalahad.trinitywizardstestapp.ui
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.rfgalahad.trinitywizardstestapp.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/rfgalahad/trinitywizardstestapp/ui/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.rfgalahad.trinitywizardstestapp.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.rfgalahad.trinitywizardstestapp.R
+
+class SecondActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
