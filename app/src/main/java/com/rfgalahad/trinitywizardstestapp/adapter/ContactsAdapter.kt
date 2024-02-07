05757296215bcd5bd1899bef9582271fa2211780
Russel Febrishky <russel.febrishky@myboost.co>
2024-02-07 15:34:13 +0800

    test unversioned files commit

---
+++ app/src/main/java/com/rfgalahad/trinitywizardstestapp/adapter/ContactsAdapter.kt
@@ -0,0 +1,24 @@
+package com.rfgalahad.trinitywizardstestapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rfgalahad.trinitywizardstestapp.R
+import com.rfgalahad.trinitywizardstestapp.holder.ContactViewHolder
+import com.rfgalahad.trinitywizardstestapp.model.User
+
+class ContactsAdapter(private val list: List<User>) : RecyclerView.Adapter<ContactViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        return ContactViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_contact, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+}
